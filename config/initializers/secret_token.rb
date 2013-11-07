2024c20680cddcd089b7861466c958a59ad066d5
james <jam@yahoo.com>
2013-11-07 09:56:05 -0800

    Start of reddit project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedditClone::Application.config.secret_token = '66d4a761405fc12b6f0245075e7cf6cc75a9338314249623c1f60211366d0095fca549978554807a7d021e6eee404177fe53690c69e211f9f19ead6c6f0fb0c5'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+RedditClone::Application.routes.draw do
+  root to: 'pages#index'
+  resources :pages
+end
